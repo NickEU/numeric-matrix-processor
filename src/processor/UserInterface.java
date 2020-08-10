891dc555756d807c95cc6d994ba8854c0fb0df50
Nick <neverate@gmail.com>
2020-08-10 08:58:28 +0300

    stage3: +main menu loop

--- src/processor/UserInterface.java
+++ src/processor/UserInterface.java
@@ -1,9 +1,55 @@
 package processor;
 
+import java.util.Optional;
 import java.util.Scanner;
 
 class UserInterface {
     private final Scanner sc = new Scanner(System.in);
+
+    public void runMainMenuLoop() {
+        while (true) {
+            System.out.print("1. Add matrices\n" +
+                "2. Multiply matrix to a constant\n" +
+                "3. Multiply matrices\n" +
+                "0. Exit\n" +
+                "Your choice: ");
+            String userChoice = sc.next();
+            switch (userChoice) {
+                case "1":
+                    menuMatrixAddition();
+                    break;
+                case "2":
+                    menuMultiplyByConstant();
+                    break;
+                case "3":
+                    menuMatrixMultiplication();
+                    break;
+                case "0":
+                    return;
+                default:
+                    System.out.println("Error! Unknown command!");
+            }
+        }
+    }
+
+    private void menuMatrixAddition() {
+        Matrix a = readMatrixFromUser();
+        Matrix b = readMatrixFromUser();
+        Optional<Matrix> result = Calculator.addMatrices(a, b);
+        String output = result.isEmpty() ? "ERROR" : result.get().toString();
+        System.out.println("\n" + output);
+    }
+
+    public void menuMultiplyByConstant() {
+        Matrix a = readMatrixFromUser();
+        int constant = sc.nextInt();
+        Matrix result = Calculator.multiplyMatrixByConst(a, constant);
+        System.out.println("\n" + result);
+    }
+
+    private void menuMatrixMultiplication() {
+        System.out.println("Multiplication happens here");
+    }
 
     private Matrix readMatrixFromUser() {
         int rows = sc.nextInt();
@@ -15,12 +61,5 @@ class UserInterface {
             }
         }
         return result;
-    }
-
-    public void runStage2() {
-        Matrix a = readMatrixFromUser();
-        int constant = sc.nextInt();
-        Matrix result = Calculator.multiplyMatrixByConst(a, constant);
-        System.out.println("\n" + result);
     }
 }
